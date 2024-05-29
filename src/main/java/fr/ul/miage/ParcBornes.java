f469b64005aef3978390936de224bf3cc4ff76ef
luksfit <luksfit.dev@gmail.com>
2024-05-29 15:03:25 +0200

    [MODIFY] Correction affichage et ajout d'un TODO dans le constructeur

--- src/main/java/fr/ul/miage/ParcBornes.java
+++ src/main/java/fr/ul/miage/ParcBornes.java
@@ -9,6 +9,7 @@ public class ParcBornes {
 
     public ParcBornes() {
         this.bornes = new ArrayList<>();
+        //TODO: il faudra récupérer les bornes existantes dans la base de données
     }
 
     public boolean ajouterBorne(Borne borne) {
@@ -19,6 +20,15 @@ public class ParcBornes {
         }
         bornes.add(borne);
         return true;
+    }
+
+    public String afficherListeBornes() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Liste des bornes :\n");
+        for (Borne borne : bornes) {
+            sb.append(borne).append("\n");
+        }
+        return sb.toString();
     }
 
     public List<Borne> getBornes() {
